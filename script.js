45037eb786740db54f17b3810e91066b7a981d43
Izzatulloh <Izzat5566@gmail.com>
2020-06-02 21:18:06 +0500

    Javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+var name = prompt('Ismingizni kiriting');
+name = 'Sizning ismingiz ' + name;
+console.log(name);
+var age = +prompt('Yoshingizni kiriting');
+age = 'Sizni yoshingiz '+age;
+console.log(age);
+var question1 = +prompt('Misolni yeching 6 + 3 =?');
+var answer1 = 6 + 3;
+question1= 'Misol 1: 6 + 3 = '+answer1 +'  Sizning javob: '+question1;
+console.log(question1);
+var question2 = +prompt('Misolni yeching 20 - 5 =?');
+var answer2 = 20 - 5;
+question2= 'Misol 2:  20 - 5 = '+answer2 +'  Sizning javob: '+question2;
+console.log(question2);
+var question3 = +prompt('Misolni yeching 15 * 2 =?');
+var answer3 = 15 * 2;
+question3= 'Misol 3:  15 * 2 = '+answer3 +'  Sizning javob: '+question3;
+console.log(question3);
+var question4 = +prompt('Misolni yeching 8 / 4 =?');
+var answer4 = 8 / 4 ;
+question4= 'Misol 4:  8 / 4 = '+answer4 +'  Sizning javob: '+question4;
+console.log(question4);
+var question5 = +prompt('Misolni yeching 10 % 3 =?');
+var answer5 = 10 % 3;
+question5= 'Misol 5:  10 % 3 = '+answer5 +'  Sizning javob: '+question5;
+console.log(question5);
+alert('Natijani korish uchun consolga kiring')
+
